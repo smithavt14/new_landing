f27c3a86d6776a7ae15af4ea13bc6da4c255f783
Alex Smith <smithavt14@gmail.com>
2019-03-11 15:43:35 +0800

    "First test with the rsync command"

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
-# ----- Variables  -----
+# Variables
 
-read -r -p 'Commit message: ' desc
-git add .
-git commit -m "$desc"
-git push origin master
+remoteUserName="deploy"
+remoteServer="moveshanghai.com"
+remotePath="/home/deploy/move-landing"
+srcDir="sourcepath/"
+
+read -r -p 'Commit message: ' desc  # prompt for commit message
+git add .                           # track all files 
+git commit -m "$desc"               # commit with message
+git push origin master              # push to origin
+
+rsync -avn ./ "$remoteUserName"@"$remoteServer":"$remotePath"
