911837044e3d760ac5be6013f179add9dd548e37
Alex Smith <smithavt14@gmail.com>
2019-03-13 10:39:23 +0800

    Changed the banner subtitle

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,8 @@
 #! /bin/bash
 
-# Variables
-
-remoteUserName="deploy"
-remoteServer="moveshanghai.com"
-remotePath="/home/deploy/move-landing"
-srcDir="sourcepath/"
-
 read -r -p 'Commit message: ' desc  # prompt for commit message
 git add .                           # track all files 
 git commit -m "$desc"               # commit with message
 git push origin master              # push to origin
 
-rsync -avn ./ "$remoteUserName"@"$remoteServer":"$remotePath"
+rsync -avn ./ deploy@moveshanghai.com:/home/deploy/move-landing
