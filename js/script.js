c0b2c51e241548e4d1330d4b0cb5e477fca5b230
Alex Smith <smithavt14@gmail.com>
2019-03-13 16:34:11 +0800

    Added a dynamic navigation bar

--- js/script.js
+++ js/script.js
@@ -3,7 +3,19 @@ var DOM = {
   hamburger: document.querySelectorAll('.mobile-navbar-icon-line'),
   exit: document.getElementById('exit'),
   mobileNavbar: document.querySelector('.mobile-navbar'),
-  mobileLink: document.querySelectorAll('.mobile-navbar-link')
+  mobileLink: document.querySelectorAll('.mobile-navbar-link'),
+  navbarContainer: document.querySelector('.navbar-container'),
+  navbarLogo: document.querySelector('.navbar-move-logo'),
+  activities: document.getElementById('activities'),
+  intro: document.getElementById('intro'),
+  venues: document.getElementById('venues'),
+  memberships: document.getElementById('memberships'),
+  access: document.getElementById('access'),
+  introLink: document.getElementById('introLink'),
+  activitiesLink: document.getElementById('activitiesLink'),
+  venuesLink: document.getElementById('venuesLink'),
+  membershipsLink:document.getElementById('membershipsLink'),
+  accessLink: document.getElementById('accessLink')
 }
 
 var toggleMobileNav = function() {
@@ -25,6 +37,36 @@ var transformIcon = function() {
   }
 }
 
+var navbarBackground = function () {
+  if (window.scrollY > window.innerHeight) {
+    DOM.navbarContainer.classList.add('navbar-background-white')
+    DOM.navbarLogo.classList.add('show')
+  } else {
+    DOM.navbarContainer.classList.remove('navbar-background-white')
+    DOM.navbarLogo.classList.remove('show')
+  }
+}
+
+var scrollToSection = function () {
+  DOM.introLink.addEventListener('click', function() {
+    window.scroll({top: DOM.intro.offsetTop + 1, behavior: 'smooth'})
+  })
+  DOM.activitiesLink.addEventListener('click', function() {
+    window.scroll({top: DOM.activities.offsetTop, behavior: 'smooth'})
+  })
+  DOM.venuesLink.addEventListener('click', function() {
+    window.scroll({top: DOM.venues.offsetTop, behavior: 'smooth'})
+  })
+  DOM.membershipsLink.addEventListener('click', function() {
+    window.scroll({top: DOM.memberships.offsetTop, behavior: 'smooth'})
+  })
+  DOM.accessLink.addEventListener('click', function() {
+    window.scroll({top: DOM.access.offsetTop, behavior: 'smooth'})
+  })
+}
+
+// Our initialize function
+
 var setupEventListeners = function () {
   DOM.menu.addEventListener('click', function() {
     transformIcon()
@@ -37,6 +79,12 @@ var setupEventListeners = function () {
       toggleMobileNav()
     })
   }
+
+  window.addEventListener('scroll', function() {
+    navbarBackground()
+  })
+
+  scrollToSection()
 }
 
 setupEventListeners()
